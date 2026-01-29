18cc01e0b03353ed2a7ac3d48f5837359bfdee22
Maksim <MaksimBorodinAndreev@gmail.com>
2026-01-29 04:48:14 +0300

    deleted all old files

--- todo_api/internal/entities/errors.go
+++
@@ -1,9 +0,0 @@
-package entities
-
-import "errors"
-
-var (
-	ErrNilDependency        = errors.New("nil dependency: ")
-	ErrSubscriptionNotFound = errors.New("subscription not found")
-	ErrTaskNotFound 		= errors.New("task not found")
-)
