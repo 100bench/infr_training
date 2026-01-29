8b9f95b792048961ae775c342ce14ae81bf698be
Maksim <MaksimBorodinAndreev@gmail.com>
2026-01-29 04:49:27 +0300

    feat: added logger and notifier to usecase

--- todo_api/internal/usecases/todo_service.go
+++ todo_api/internal/usecases/todo_service.go
@@ -2,25 +2,41 @@ package usecases
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"time"
-	"encoding/json"
 
+	er "github.com/100bench/infr_training/pkg/errors"
+	"github.com/100bench/infr_training/pkg/logger"
+	pb "github.com/100bench/infr_training/pkg/proto/gen/notification/v1"
 	"github.com/100bench/infr_training/todo_api/internal/entities"
 )
 
 type TodoService struct {
 	storage TaskStorage
 	cache Cache
+	notifier Notifier
+	log logger.Logger
 }
 
-func NewTodoService(storage TaskStorage, cache Cache) (*TodoService, error) {
+func NewTodoService(storage TaskStorage, cache Cache, notifier Notifier, logger logger.Logger) (*TodoService, error) {
 	if storage == nil {
-		return nil, fmt.Errorf("NewTodoService: %w", entities.ErrNilDependency)
+		return nil, fmt.Errorf("NewTodoService storage: %w", er.ErrNilDependency)
+	}
+	if cache == nil {
+		return nil, fmt.Errorf("NewTodoService cache: %w", er.ErrNilDependency)
+	}
+	if notifier == nil {
+		return nil, fmt.Errorf("NewTodoService notifier: %w", er.ErrNilDependency)
+	}
+	if logger == nil {
+		return nil, fmt.Errorf("NewTodoService logger: %w", er.ErrNilDependency)
 	}
 	return &TodoService{
 		storage: storage,
 		cache: cache,
+		notifier: notifier,
+		log: logger,
 	}, nil
 }
 
@@ -31,10 +47,17 @@ func (s *TodoService) CreateTask(ctx context.Context, title, description string)
 	if err != nil {
 		return nil, err
 	}
+
+	// sircuit breaker pattern can be applied here
+	err = s.notifier.Send(ctx, task.ID.String(), title, pb.EventType_EVENT_TYPE_CREATED)
+	if err != nil{
+		s.log.Warn("failed to send create notification")
+	}
+
 	return task, nil
 }
 
-func (s *TodoService) GetTasks(ctx context.Context, id string) (*entities.Task, error) {
+func (s *TodoService) GetTask(ctx context.Context, id string) (*entities.Task, error) {
 	key := fmt.Sprintf("task:%s", id)
 
 	// Try cache first
@@ -61,11 +84,17 @@ func (s *TodoService) GetTasks(ctx context.Context, id string) (*entities.Task, 
 }
 
 func (s *TodoService) DeleteTask(ctx context.Context, id string) error {
-	err := s.storage.Delete(ctx, id)
+	title, err := s.storage.Delete(ctx, id)
 	if err != nil {
 		return err
 	}
-	// Invalidate cache after successful delete
+
+	// sircuit breaker pattern can be applied here
+	err = s.notifier.Send(ctx, id, title, pb.EventType_EVENT_TYPE_DELETED)
+	if err != nil{
+		s.log.Warn("failed to send delete notification")
+	}
+	
 	if s.cache != nil {
 		key := fmt.Sprintf("task:%s", id)
 		s.cache.Delete(ctx, key)
